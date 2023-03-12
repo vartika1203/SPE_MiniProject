b68a5dc91abf3b393c1e1fa0c90ee05f9b3cc962
vartika1203 <vartikach02@gmail.com>
2023-03-12 18:43:26 +0530

    created a new maven project

---
+++ src/main/java/com/miniproject/App.java
@@ -0,0 +1,13 @@
+package com.miniproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
